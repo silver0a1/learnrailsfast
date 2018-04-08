a8b14c15afbba570cad1e45305ace20666c9d38d
silver0a1 <ryan@aperturesec.com>
2018-04-08 23:44:11 +0000

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-      render text: "hello world!"
+      #render text: "hello world!" can only have one render per view
+      render text: "<h1>Whatup</h1>"
   end
 end
